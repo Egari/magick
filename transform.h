65d2f78df7989115295d83a2f260e64d3ea9cba6
Roald Stolte <stolter@transip.nl>
2016-08-24 14:52:07 +0200

    Backport AutoOrientImage() function

--- transform.h
+++ transform.h
@@ -3,6 +3,6 @@
 
 Image *flattenImages(const Image *image, ExceptionInfo *exception);
 Image *mosaicImages(const Image *image, ExceptionInfo *exception);
-
+Image *autoOrientImage(Image *image, void *orientation, ExceptionInfo *ex);
 
 #endif
